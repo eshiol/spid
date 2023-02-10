d9164865666324c995c53973b9ad35eb1ec45fd8
Helios Ciancio <12718836+eshiol@users.noreply.github.com>
2023-02-10 08:17:53 +0100

    chore: copyright

--- plugins/system/spid/spid.php
+++ plugins/system/spid/spid.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package     Joomla.Plugins
+ * @package     Joomla.Plugin
  * @subpackage  System.SPiD
  *
  * @version     __DEPLOY_VERSION__
@@ -8,12 +8,12 @@
  *
  * @author      Helios Ciancio <info (at) eshiol (dot) it>
  * @link        https://www.eshiol.it
- * @copyright   Copyright (C) 2017 - 2022 Helios Ciancio. All rights reserved
+ * @copyright   Copyright (C) 2017 - 2023 Helios Ciancio. All rights reserved.
  * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL v3
- * SPiD  for  Joomla!  is  free software. This version may have been modified
- * pursuant to the GNU General Public License, and as distributed it includes
- * or is derivative of works licensed under the GNU General Public License or
- * other free or open source software licenses.
+ * Joomla.Plugin.System.SPiD  is  free  software.  This  version may have been
+ * modified pursuant to the GNU General Public License,  and as distributed it
+ * includes  or  is  derivative of works licensed under the GNU General Public 
+ * License or other free or open source software licenses.
  */
 
 defined('_JEXEC') or die();
